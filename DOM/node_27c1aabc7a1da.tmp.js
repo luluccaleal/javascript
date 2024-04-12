458e7ac5b9240e14052beb6d441bce0e030ebc35
luluccaleal <luciana.b.oliveira6@aluno.senai.br>
2024-04-12 10:51:11 -0300

    condicionais

---
+++ DOM/node_27c1aabc7a1da.tmp.js
@@ -0,0 +1,10 @@
+
+
+var  país  =  'Brasil'
+console . log ( `Vivendo no ${ país } ` )
+if  ( país  ==  'Brasil' ) {
+    console . log ( 'Você é Brasileiro!' )  //Verdadeiro
+}
+outro  {
+    console . log ( 'Você é estrangeiro!' )  //Falso
+}
