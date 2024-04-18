c6cc851d727c251f94deaa3cf4d04ebb7c9e9d79
luluccaleal <luciana.b.oliveira6@aluno.senai.br>
2024-04-18 10:51:02 -0300

    repetições

---
+++ DOM/repeticoes/exercicio.js
@@ -0,0 +1,14 @@
+//console.log('Tem fatia?')
+//console.log('Tem fatia?')
+
+//var c = 1
+//while (c <= 8) {
+//console.log('Tem fatia?')
+// c++ // c = c + 1
+//}
+
+var c = 1
+while (c <= 8) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
---
+++ DOM/repeticoes/exercicio2.js
@@ -0,0 +1,11 @@
+//var c = 1
+//do {
+  //  console.log(`Passo ${c}`)
+    //c++ // c = 1 + 1
+//} while (c <= 8)
+
+console.log('Início')
+for (var c = 1; c <=10; c++) {
+console.log(c)
+}
+console.log('Fim!')
